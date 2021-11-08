a1fec6ebefba2f6cd5b3dfcb1e859b7abb43fabb
Andrew Shevchov <jokey102020@gmail.com>
2021-11-08 04:56:47 +0300

    rev8

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,57 @@
-const restourant = 'food-band'
+const cardsMenu = document.querySelector('.cards-menu')
+const sectionHeading = document.querySelector('.section-heading') 
 
-fetch(`./db/${restourant}.json`)
+const changeTitle = (restaraunt) =>
+{
+const restaurantTitle = document.querySelector('.restaurant-title') 
+
+ restaurantTitle.textContent=restaraunt.name
+ 
+}
+
+
+
+const renderItems = (data)=>{
+
+  data.forEach(({description,id,image,name, price}) => {
+      const card = document.createElement('div')
+
+      card.classList.add('card')
+    
+     card.innerHTML = `
+        <img src="${image}" alt="${name}" class="card-image" />
+                            <div class="card-text">
+                                <div class="card-heading">
+                                    <h3 class="card-title card-title-reg">${name}</h3>
+                                </div>
+            
+                                <div class="card-info">
+                                    <div class="ingredients">
+                                    ${description}
+                                    </div>
+                                </div>
+                               
+                                <div class="card-buttons">
+                                    <button class="button button-primary button-add-cart">
+                                        <span class="button-card-text">В корзину</span>
+                                        <span class="button-cart-svg"></span>
+                                    </button>
+                                    <strong class="card-price-bold">${price}₽</strong>
+                                </div>
+                            </div>      
+        `
+        cardsMenu.append(card)
+  })
+    
+}
+
+if (localStorage.getItem('restaraunt')){
+
+const restaraunt = JSON.parse(localStorage.getItem('restaraunt'))
+
+changeTitle(restaraunt)
+
+fetch(`./db/${restaraunt.products}`)
 .then((response)=>response.json())
 .then((data)=>{
     renderItems(data)
@@ -8,8 +59,11 @@ fetch(`./db/${restourant}.json`)
 .catch((error)=>{
     console.log(error)
 })
-
-const renderItems = (data)=>{
-    console.log(data)
-    
 }
+else{
+    window.location.href = '/'
+}
+
+
+
+
